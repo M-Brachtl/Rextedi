9782be5c4e4e38c13a9063b7c1b1a04d30315c29
Matěj Brachtl <bra42176@spseol.cz>
2026-02-19 22:08:28 +0100

    Correct UNICODE handling

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use crossterm::{
     }
 };
 
+use unicode_segmentation::UnicodeSegmentation;
+
 static HEADER_HEIGHT: u16 = 3;
 static DEBUG_MODE: bool = false;
 
@@ -27,7 +29,6 @@ fn main() -> std::io::Result<()> {
         println!("Rextedi: No file path");
         return Ok(());
     }
-
     // Enable raw mode
     enable_raw_mode()?;
     execute!(stdout, Clear(ClearType::All), EnterAlternateScreen, Hide)?;
@@ -84,12 +85,12 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
     } else {
         None
     };
-
+    // fs::write(Path::new("./debug.txt"), lines.last().unwrap_or(&String::from("not enough chars")).escape_debug().to_string())?;
     init_draw(stdout, &lines, width, height)?;
     loop {
         draw(stdout, &lines[active_line], cursor, width as usize, hor_offset)?;
         if DEBUG_MODE {
-            debug_log.as_ref().unwrap().draw_debug(stdout, &format!("Debug log: ActiveLine: {}, Cursor: ({}, {}), HorOffset: {}, VerOffset: {}", active_line, cursor.0, cursor.1, hor_offset, ver_offset), height, cursor)?;
+            debug_log.as_ref().unwrap().draw_debug(stdout, &format!("Debug log: ActiveLine: {}, Cursor: ({}, {}), HorOffset: {}, VerOffset: {}, LineLen: {}, CurCharRepr: {}", active_line, cursor.0, cursor.1, hor_offset, ver_offset, lines[active_line].graphemes(true).count(), lines[active_line].graphemes(true).nth((cursor.0 + hor_offset) as usize).unwrap_or("\0").escape_debug()), height, cursor)?;
         }
         if let Some(log_instance) = &log {
             if log_instance.timestamp.elapsed().unwrap().as_secs() < 5 {
@@ -105,7 +106,7 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                 match key.code {
                     KeyCode::Char('q') => if key.modifiers.contains(crossterm::event::KeyModifiers::CONTROL) { execute!(stdout, MoveTo(0, 0), LeaveAlternateScreen)?; break; } else {
                         // lines[active_line].push('q');
-                        write_char(&mut lines[active_line], 'q', &mut cursor.0);
+                        write_char(&mut lines[active_line], 'q', &mut cursor.0, &hor_offset);
                     },
                     KeyCode::Char('w') => if key.modifiers.contains(crossterm::event::KeyModifiers::CONTROL) {
                         if let Err(_) = save_file(file_name, lines.join(if crlf {"\r\n"} else {"\n"}), &mut log) {
@@ -128,14 +129,14 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                             }
                         }
                     } else {
-                        write_char(&mut lines[active_line], 'w', &mut cursor.0);
+                        write_char(&mut lines[active_line], 'w', &mut cursor.0, &hor_offset);
                     }
-                    KeyCode::Char(c) => if !key.modifiers.contains(KeyModifiers::CONTROL) { write_char(&mut lines[active_line], c, &mut cursor.0); }
+                    KeyCode::Char(c) => if !key.modifiers.contains(KeyModifiers::CONTROL) { write_char(&mut lines[active_line], c, &mut cursor.0, &hor_offset); }
                     KeyCode::Backspace => {
                         if cursor.0 > 0 {
-                            remove_char(&mut lines[active_line], &mut cursor.0);
+                            remove_char(&mut lines[active_line], &mut cursor.0, &hor_offset);
                             execute!(stdout, MoveTo(cursor.0, cursor.1 + HEADER_HEIGHT))?;
-                        } else if cursor.1 > 0{
+                        } else if cursor.1 > 0 && hor_offset == 0 {
                             let removed_line_content = lines[active_line].clone();
                             lines[active_line-1].push_str(&removed_line_content);
                             lines.remove(active_line);
@@ -145,23 +146,25 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                             }
                             active_line -= 1;
                             draw_new_line(stdout, &lines, cursor, width as usize, height, ver_offset)?;
-                            cursor.0 = lines[active_line].len() as u16 - removed_line_content.len() as u16;
+                            cursor.0 = lines[active_line].graphemes(true).count() as u16 - removed_line_content.graphemes(true).count() as u16;
                             cursor.1 -= 1;
                         }
                     }
                     KeyCode::Enter => {
-                        let new_line_content = lines[active_line].split_off((cursor.0 + hor_offset) as usize);
+                        let byte_index = lines[active_line].grapheme_indices(true).nth((cursor.0 + hor_offset) as usize).map(|(i, _)| i).unwrap_or(lines[active_line].len());
+                        let new_line_content = lines[active_line].split_off(byte_index);
                         lines.insert(active_line + 1, new_line_content);
+                        draw_new_line(stdout, &lines, cursor, width as usize, height, ver_offset)?;
                         if cursor.1 + 1 == height as u16 {
                             ver_offset += 1;
                             move_text(ver_offset, &lines, cursor, stdout, width, height)?;
                             cursor.1 = height as u16 - 1;
+                        } else {
+                            cursor.1 += 1;
                         }
-                        draw_new_line(stdout, &lines, cursor, width as usize, height, ver_offset)?;
                         hor_offset = 0;
                         active_line += 1;
                         cursor.0 = 0;
-                        cursor.1 += 1;
                     },
                     KeyCode::Left => {
                         if key.modifiers.contains(KeyModifiers::CONTROL) {
@@ -176,7 +179,7 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                     KeyCode::Right => {
                         if key.modifiers.contains(KeyModifiers::CONTROL) {
                             hor_offset += 1;
-                        } else if cursor.0 < lines[active_line].len() as u16 {
+                        } else if cursor.0 + hor_offset < lines[active_line].graphemes(true).count() as u16 {
                             cursor.0 += 1;
                             execute!(stdout, MoveTo(cursor.0, cursor.1 + HEADER_HEIGHT))?;
                         }
@@ -194,8 +197,8 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                                 cursor.1 -= 1;
                             }
                             active_line -= 1;
-                            if cursor.0 > lines[active_line].len() as u16 {
-                                cursor.0 = lines[active_line].len() as u16;
+                            if cursor.0 + hor_offset > lines[active_line].graphemes(true).count() as u16 {
+                                cursor.0 = lines[active_line].graphemes(true).count() as u16 - hor_offset;
                             }
                             execute!(stdout, MoveTo(cursor.0, cursor.1 + HEADER_HEIGHT))?;
                         }
@@ -207,8 +210,8 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
                             // draw_new_line(stdout, &lines, cursor, width as usize, height, ver_offset)?;
                             active_line += 1;
                             cursor.1 += 1;
-                            if cursor.0 > lines[active_line].len() as u16 {
-                                cursor.0 = lines[active_line].len() as u16;
+                            if cursor.0 + hor_offset > lines[active_line].graphemes(true).count() as u16 {
+                                cursor.0 = lines[active_line].graphemes(true).count() as u16 - hor_offset;
                             } else if cursor.1 >= height as u16 {
                                 ver_offset += 1;
                                 move_text(ver_offset, &lines, cursor, stdout, width, height)?;
@@ -239,7 +242,7 @@ fn run(stdout: &mut std::io::Stdout, starting_text: String, file_name: &Path) ->
 fn init_draw(stdout: &mut std::io::Stdout, lines: &Vec<String>, max_width: usize, max_height: usize) -> std::io::Result<()> {
     let mut display_lines = Vec::new();
     for line in lines.iter().take(max_height) {
-        if line.len() > max_width {
+        if line.graphemes(true).count() > max_width {
             display_lines.push(line.split_at(max_width).0.to_string());
         } else {
             display_lines.push(line.clone());
@@ -263,8 +266,8 @@ fn init_draw(stdout: &mut std::io::Stdout, lines: &Vec<String>, max_width: usize
 }
 
 fn draw(stdout: &mut std::io::Stdout, line: &String, cursor_pos: (u16, u16), max_width: usize, offset: u16) -> std::io::Result<()> {
-    let offset_bytes = line.char_indices().nth(offset as usize).map(|(i, _)| i).unwrap_or(line.len());
-    let display_line = line[offset_bytes..].chars().take(max_width).collect::<String>();
+    let offset_bytes = line.grapheme_indices(true).nth(offset as usize).map(|(i, _)| i).unwrap_or(line.graphemes(true).count());
+    let display_line = line[offset_bytes..].graphemes(true).take(max_width).collect::<String>();
     queue!(
         stdout,
         crossterm::cursor::Hide,
@@ -279,14 +282,23 @@ fn draw(stdout: &mut std::io::Stdout, line: &String, cursor_pos: (u16, u16), max
 }
 
 fn draw_new_line(stdout: &mut std::io::Stdout, lines: &Vec<String>, cursor_pos: (u16, u16), max_width: usize, max_height: usize, ver_offset: u16) -> std::io::Result<()> {
-    let (_, moved_lines) = lines.split_at(cursor_pos.1 as usize);
+    let (_, moved_lines) = lines.split_at((cursor_pos.1 + ver_offset) as usize);
     queue!(stdout, crossterm::cursor::Hide, Clear(ClearType::FromCursorDown))?;
-    for (index, line) in moved_lines[ver_offset as usize..].iter().take(max_height - cursor_pos.1 as usize).enumerate(){
+    if ver_offset > moved_lines.len() as u16 {
+        queue!(
+            stdout,
+            MoveTo(0, cursor_pos.1 + HEADER_HEIGHT),
+            // Clear(ClearType::CurrentLine),
+        )?;
+        stdout.flush()?;
+        return Ok(()); // No need to redraw if the new line is outside the current view
+    }
+    for (index, line) in moved_lines[ver_offset as usize..].iter().take(max_height - (cursor_pos.1 + ver_offset) as usize).enumerate(){
         queue!(
             stdout,
             MoveTo(0, cursor_pos.1 + index as u16 + HEADER_HEIGHT),
             Clear(ClearType::CurrentLine),
-            Print(if line.len() > max_width {
+            Print(if line.graphemes(true).count() > max_width {
                 line.split_at(max_width).0
             } else {
                 line
@@ -302,14 +314,14 @@ fn draw_new_line(stdout: &mut std::io::Stdout, lines: &Vec<String>, cursor_pos: 
     Ok(())
 }
 
-fn write_char(line: &mut String, c: char, cursor_x: &mut u16) {
-    let byte_index = line.char_indices().nth(*cursor_x as usize).map(|(i, _)| i).unwrap_or(line.len());
+fn write_char(line: &mut String, c: char, cursor_x: &mut u16, hor_offset: &u16) {
+    let byte_index = line.grapheme_indices(true).nth((*cursor_x + *hor_offset) as usize).map(|(i, _)| i).unwrap_or(line.len()); // at the end has to be len to get byte index, not grapheme index
     line.insert(byte_index, c);
     *cursor_x += 1;
 }
 
-fn remove_char(line: &mut String, cursor_x: &mut u16) {
-    let byte_index = line.char_indices().nth(*cursor_x as usize - 1).map(|(i, _)| i).unwrap_or(line.len());
+fn remove_char(line: &mut String, cursor_x: &mut u16, hor_offset: &u16) {
+    let byte_index = line.grapheme_indices(true).nth((*cursor_x + *hor_offset) as usize - 1).map(|(i, _)| i).unwrap_or(line.len()); // at the end has to be len to get byte index, not grapheme index
     line.remove(byte_index);
     *cursor_x -= 1;
 }
@@ -322,7 +334,7 @@ fn move_text(ver_offset: u16, lines: &Vec<String>, cursor_pos: (u16, u16), stdou
             stdout,
             MoveTo(0, index as u16 + HEADER_HEIGHT),
             Clear(ClearType::CurrentLine),
-            Print(if line.len() > width {
+            Print(if line.graphemes(true).count() > width {
                 line.split_at(width).0
             } else {
                 line
